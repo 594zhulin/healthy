34ba7fefa4ba4145caab8b09e964265c6c3ef19c
zhulin <YS.zhulin@h3c.com>
2020-12-08 21:51:45 +0800

    个人中心

---
+++ pages/measure/history.vue
@@ -0,0 +1,96 @@
+<template>
+	<view class="page-history">
+		<view class="list-content">
+			<view class="list-item">
+				<view class="score">
+					<view class="tag">
+						<image class="icon" src="../../static/measure/measure-icon-13.png" mode="aspectFit"></image>
+						<view class="text">优</view>
+					</view>
+					<view class="text">93.2分</view>
+				</view>
+				<view class="date">
+					<view class="text">2019.06.02 12:00</view>
+					<image class="icon" src="../../static/measure/measure-icon-14.png" mode="aspectFit"></image>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less">
+page {
+	background-color: #f7f6f9;
+}
+.page-history {
+	.list-content {
+		.list-item {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			height: 164rpx;
+			margin: 30rpx;
+			padding: 0 36rpx;
+			background: #ffffff;
+			border-radius: 16rpx;
+			.score {
+				display: flex;
+				align-items: center;
+				& > .text {
+					margin-left: 14rpx;
+					font-size: 24rpx;
+					font-family: PingFangSC-Medium, PingFang SC;
+					font-weight: 500;
+					color: #2975ff;
+					line-height: 26rpx;
+				}
+				.tag {
+					position: relative;
+					width: 72rpx;
+					height: 78rpx;
+					.icon {
+						position: absolute;
+						top: 0;
+						left: 0;
+						width: 72rpx;
+						height: 78rpx;
+					}
+					.text {
+						position: relative;
+						font-size: 48rpx;
+						font-family: PingFangSC-Medium, PingFang SC;
+						font-weight: 500;
+						color: #ffffff;
+						line-height: 78rpx;
+						text-align: center;
+					}
+				}
+			}
+			.date {
+				display: flex;
+				align-items: center;
+				.text {
+					margin-right: 52rpx;
+					font-size: 28rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #999999;
+					line-height: 38rpx;
+				}
+				.icon {
+					width: 19rpx;
+					height: 30rpx;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ pages/personal/index.vue
@@ -0,0 +1,160 @@
+<template>
+	<view class="page-index">
+		<view class="avatar-content">
+			<view class="label">头像</view>
+			<view class="avatar"></view>
+			<image class="icon" src="../../static/train/train-icon-05.png" mode="aspectFit"></image>
+		</view>
+		<view class="name-content">
+			<view class="label">姓名</view>
+			<input class="text" type="text" v-model="name" />
+		</view>
+		<view class="sex-content">
+			<view class="label">性别</view>
+			<picker class="picker" @change="bindTagChange" :value="index" :range="array">
+				<view class="text">{{ array[index] }}</view>
+			</picker>
+			<image class="icon" src="../../static/train/train-icon-05.png" mode="aspectFit"></image>
+		</view>
+		<view class="birthday-content">
+			<view class="label">出生日期</view>
+			<picker class="picker" mode="date" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
+				<view class="text">{{ date }}</view>
+			</picker>
+			<image class="icon" src="../../static/train/train-icon-05.png" mode="aspectFit"></image>
+		</view>
+		<view class="btn">确认修改</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		const currentDate = this.getDate({
+			format: true
+		});
+		return {
+			name: '',
+			array: ['男', '女'],
+			index: 0,
+			date: currentDate
+		};
+	},
+	computed: {
+		startDate() {
+			return this.getDate('start');
+		},
+		endDate() {
+			return this.getDate('end');
+		}
+	},
+	methods: {
+		bindDateChange: function(e) {
+			this.date = e.target.value;
+		},
+		bindPickerChange: function(e) {
+			this.index = e.target.value;
+		},
+		getDate(type) {
+			const date = new Date();
+			let year = date.getFullYear();
+			let month = date.getMonth() + 1;
+			let day = date.getDate();
+
+			if (type === 'start') {
+				year = year - 60;
+			} else if (type === 'end') {
+				year = year + 2;
+			}
+			month = month > 9 ? month : '0' + month;
+			day = day > 9 ? day : '0' + day;
+			return `${year}-${month}-${day}`;
+		}
+	}
+};
+</script>
+
+<style lang="less">
+.page-index {
+	padding-top: 30rpx;
+	background-color: #f7f6f9;
+	.avatar-content,
+	.name-content,
+	.sex-content,
+	.birthday-content {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		height: 96rpx;
+		margin: 0 0 30rpx 0;
+		padding: 0 32rpx 0 60rpx;
+		background-color: #ffffff;
+		.label {
+			font-size: 28rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #333333;
+			line-height: 40rpx;
+		}
+		.avatar {
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			right: 76rpx;
+			width: 80rpx;
+			height: 80rpx;
+			margin: auto;
+			border-radius: 100%;
+			background-color: #eee;
+		}
+		.picker {
+			flex: 1;
+			height: 96rpx;
+			line-height: 96rpx;
+		}
+		.text,
+		.input {
+			flex: 1;
+			height: 96rpx;
+			line-height: 96rpx;
+			font-size: 28rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #999999;
+			text-align: right;
+		}
+		.icon {
+			width: 19rpx;
+			height: 30rpx;
+			margin-left: 26rpx;
+		}
+	}
+	.birthday-content::after {
+		content: '';
+		position: absolute;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		height: 2rpx;
+		background-color: #e8e8e8;
+	}
+	.btn {
+		position: absolute;
+		left: 0;
+		right: 0;
+		bottom: 102rpx;
+		width: 226rpx;
+		height: 72rpx;
+		margin: 0 auto;
+		background: #2975ff;
+		border-radius: 4rpx;
+		font-size: 28rpx;
+		font-family: PingFangSC-Semibold, PingFang SC;
+		font-weight: 600;
+		color: #ffffff;
+		line-height: 72rpx;
+		text-align: center;
+	}
+}
+</style>
