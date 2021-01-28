b4f56047a41cd2dc730293c7d75bea34924cb16c
zhulin <zhul@tcfuture.tech>
2021-01-28 11:15:55 +0800

    bug修复

--- pages/order/aftersale.vue
+++ pages/order/aftersale.vue
@@ -4,7 +4,8 @@
 			<view class="list-item" v-for="item in order" :key="item.id" @click="navigateTo('/pages/order/detail?id=' + item.order_id)">
 				<view class="title">
 					<view class="number">订单编号：{{ item.order_id }}</view>
-					<view class="status" v-if="item.status == -3">退款中</view>
+					<view class="status" v-if="item._status._type == -1">退款中</view>
+					<view class="status" v-if="item._status._type == -2">已退款</view>
 				</view>
 				<view class="product" v-for="cart in item.cartInfo" :key="cart.id">
 					<image class="pic" :src="cart.productInfo.image" mode="aspectFit"></image>
@@ -22,7 +23,7 @@
 						<view class="text">{{ item.total_price }}</view>
 					</view>
 					<view class="pay">
-						<view class="label">{{ item.status == 0 ? '需付款' : '实付款' }}</view>
+						<view class="label">实付款</view>
 						<view class="text">{{ item.pay_price }}</view>
 					</view>
 				</view>
@@ -32,173 +33,222 @@
 </template>
 
 <script>
-import { getOrder } from '@/api/order.js';
-export default {
-	data() {
-		return {
-			params: {
-				type: '-3',
-				page: 1,
-				limit: 20
-			},
-			order: [],
-			total: 0
-		};
-	},
-	onShow() {
-		this.getListData('down');
-	},
-	onReachBottom() {
-		this.getListData('up');
-	},
-	methods: {
-		getListData(direction) {
-			if (direction == 'down') {
-				this.params.page = 1;
-			} else {
-				if (this.order.length >= this.total) {
-					return false;
-				}
-				this.params.page += 1;
-			}
-			getOrder({ ...this.params }).then(
-				result => {
-					const { list, total } = result;
-					this.order = direction == 'down' ? list : this.order.concat(list);
-					this.total = total;
+	import {
+		getOrder
+	} from '@/api/order.js';
+	export default {
+		data() {
+			return {
+				params: {
+					type: '-3',
+					page: 1,
+					limit: 20
 				},
-				err => {}
-			);
+				order: [],
+				total: 0
+			};
 		},
-		navigateTo(url) {
-			uni.navigateTo({
-				url
-			});
+		onShow() {
+			this.getListData('down');
+		},
+		onReachBottom() {
+			this.getListData('up');
+		},
+		methods: {
+			getListData(direction) {
+				if (direction == 'down') {
+					this.params.page = 1;
+				} else {
+					if (this.order.length >= this.total) {
+						return false;
+					}
+					this.params.page += 1;
+				}
+				getOrder({ ...this.params
+				}).then(
+					result => {
+						const {
+							list,
+							total
+						} = result;
+						this.order = direction == 'down' ? list : this.order.concat(list);
+						this.total = total;
+					},
+					err => {}
+				);
+			},
+			navigateTo(url) {
+				uni.navigateTo({
+					url
+				});
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="less">
-page {
-	background-color: #f7f6f9;
-}
-.page-aftersale {
-	.list-content {
-		.list-item {
-			position: relative;
-			margin: 30rpx;
-			background: #ffffff;
-			border-radius: 16rpx;
-			.title {
+	page {
+		background-color: #f7f6f9;
+	}
+
+	.page-aftersale {
+		.list-content {
+			.list-item {
 				position: relative;
-				display: flex;
-				align-items: center;
-				justify-content: space-between;
-				height: 80rpx;
-				padding: 0 30rpx;
-				&::after {
-					content: '';
-					position: absolute;
-					left: 0;
-					right: 0;
-					bottom: 0;
-					height: 1rpx;
-					background-color: #e8e8e8;
-				}
-				.number {
-					font-size: 28rpx;
-					font-family: PingFangSC-Regular, PingFang SC;
-					font-weight: 400;
-					color: #333;
-					line-height: 40rpx;
-				}
-				.status {
-					font-size: 28rpx;
-					font-family: PingFangSC-Regular, PingFang SC;
-					font-weight: 400;
-					color: #2975ff;
-					line-height: 40rpx;
-				}
-			}
-			.product {
-				display: flex;
-				align-items: center;
-				padding: 30rpx;
-				.pic {
-					flex-shrink: 0;
-					width: 120rpx;
-					height: 120rpx;
-					margin-right: 30rpx;
-					border-radius: 16rpx;
-					background-color: #eee;
-				}
-				.detail {
+				margin: 30rpx;
+				background: #ffffff;
+				border-radius: 16rpx;
+
+				.title {
 					position: relative;
 					display: flex;
-					flex-direction: column;
+					align-items: center;
 					justify-content: space-between;
-					flex: 1;
-					height: 120rpx;
-					.name {
-						width: 70%;
+					height: 80rpx;
+					padding: 0 30rpx;
+
+					&::after {
+						content: '';
+						position: absolute;
+						left: 0;
+						right: 0;
+						bottom: 0;
+						height: 1rpx;
+						background-color: #e8e8e8;
+					}
+
+					.number {
 						font-size: 28rpx;
 						font-family: PingFangSC-Regular, PingFang SC;
 						font-weight: 400;
 						color: #333;
 						line-height: 40rpx;
-						word-break: break-all;
-						text-overflow: ellipsis;
-						display: -webkit-box;
-						-webkit-box-orient: vertical;
-						-webkit-line-clamp: 2;
-						overflow: hidden;
 					}
-					.specs {
-						font-size: 24rpx;
-						font-family: PingFangSC-Regular, PingFang SC;
-						font-weight: 400;
-						color: #999;
-						line-height: 34rpx;
-					}
-					.count {
-						position: absolute;
-						bottom: 0;
-						right: 0;
-						font-size: 24rpx;
-						font-family: PingFangSC-Regular, PingFang SC;
-						font-weight: 400;
-						color: #999;
-						line-height: 34rpx;
-					}
-					.step {
-						position: absolute;
-						top: 0;
-						right: 0;
+
+					.status {
 						font-size: 28rpx;
 						font-family: PingFangSC-Regular, PingFang SC;
 						font-weight: 400;
-						color: #999;
+						color: #2975ff;
 						line-height: 40rpx;
-						&::after {
-							content: '步';
-							font-size: 18rpx;
+					}
+				}
+
+				.product {
+					display: flex;
+					align-items: center;
+					padding: 30rpx;
+
+					.pic {
+						flex-shrink: 0;
+						width: 120rpx;
+						height: 120rpx;
+						margin-right: 30rpx;
+						border-radius: 16rpx;
+						background-color: #eee;
+					}
+
+					.detail {
+						position: relative;
+						display: flex;
+						flex-direction: column;
+						justify-content: space-between;
+						flex: 1;
+						height: 120rpx;
+
+						.name {
+							width: 70%;
+							font-size: 28rpx;
+							font-family: PingFangSC-Regular, PingFang SC;
+							font-weight: 400;
+							color: #333;
+							line-height: 40rpx;
+							word-break: break-all;
+							text-overflow: ellipsis;
+							display: -webkit-box;
+							-webkit-box-orient: vertical;
+							-webkit-line-clamp: 2;
+							overflow: hidden;
+						}
+
+						.specs {
+							font-size: 24rpx;
 							font-family: PingFangSC-Regular, PingFang SC;
 							font-weight: 400;
 							color: #999;
-							line-height: 26rpx;
+							line-height: 34rpx;
+						}
+
+						.count {
+							position: absolute;
+							bottom: 0;
+							right: 0;
+							font-size: 24rpx;
+							font-family: PingFangSC-Regular, PingFang SC;
+							font-weight: 400;
+							color: #999;
+							line-height: 34rpx;
+						}
+
+						.step {
+							position: absolute;
+							top: 0;
+							right: 0;
+							font-size: 28rpx;
+							font-family: PingFangSC-Regular, PingFang SC;
+							font-weight: 400;
+							color: #999;
+							line-height: 40rpx;
+
+							&::after {
+								content: '步';
+								font-size: 18rpx;
+								font-family: PingFangSC-Regular, PingFang SC;
+								font-weight: 400;
+								color: #999;
+								line-height: 26rpx;
+							}
+						}
+
+						.text {
+							position: absolute;
+							top: 0;
+							right: 0;
+							font-size: 28rpx;
+							font-family: PingFangSC-Regular, PingFang SC;
+							font-weight: 400;
+							color: #999;
+							line-height: 40rpx;
+
+							&::before {
+								content: '￥';
+								font-size: 24rpx;
+								font-family: PingFangSC-Regular, PingFang SC;
+								font-weight: 400;
+								color: #999;
+								line-height: 34rpx;
+							}
 						}
 					}
-					.text {
-						position: absolute;
-						top: 0;
-						right: 0;
-						font-size: 28rpx;
+				}
+
+				.price {
+					display: flex;
+					align-items: center;
+					padding: 0 30rpx 20rpx 0;
+					justify-content: flex-end;
+
+					.total {
+						display: flex;
+						align-items: center;
+						margin-right: 20rpx;
+						font-size: 32rpx;
 						font-family: PingFangSC-Regular, PingFang SC;
 						font-weight: 400;
 						color: #999;
-						line-height: 40rpx;
-						&::before {
+						line-height: 46rpx;
+
+						.text::before {
 							content: '￥';
 							font-size: 24rpx;
 							font-family: PingFangSC-Regular, PingFang SC;
@@ -207,50 +257,27 @@ page {
 							line-height: 34rpx;
 						}
 					}
-				}
-			}
-			.price {
-				display: flex;
-				align-items: center;
-				padding: 0 30rpx 20rpx 0;
-				justify-content: flex-end;
-				.total {
-					display: flex;
-					align-items: center;
-					margin-right: 20rpx;
-					font-size: 32rpx;
-					font-family: PingFangSC-Regular, PingFang SC;
-					font-weight: 400;
-					color: #999;
-					line-height: 46rpx;
-					.text::before {
-						content: '￥';
-						font-size: 24rpx;
-						font-family: PingFangSC-Regular, PingFang SC;
-						font-weight: 400;
-						color: #999;
-						line-height: 34rpx;
-					}
-				}
-				.pay {
-					display: flex;
-					align-items: center;
-					font-size: 32rpx;
-					font-family: PingFangSC-Regular, PingFang SC;
-					font-weight: 400;
-					color: #fc6262;
-					line-height: 46rpx;
-					.text::before {
-						content: '￥';
-						font-size: 24rpx;
+
+					.pay {
+						display: flex;
+						align-items: center;
+						font-size: 32rpx;
 						font-family: PingFangSC-Regular, PingFang SC;
 						font-weight: 400;
 						color: #fc6262;
-						line-height: 34rpx;
+						line-height: 46rpx;
+
+						.text::before {
+							content: '￥';
+							font-size: 24rpx;
+							font-family: PingFangSC-Regular, PingFang SC;
+							font-weight: 400;
+							color: #fc6262;
+							line-height: 34rpx;
+						}
 					}
 				}
 			}
 		}
 	}
-}
 </style>
--- pages/order/refund.vue
+++ pages/order/refund.vue
@@ -13,7 +13,7 @@
 		<view class="reason-content">
 			<view class="label">退款原因</view>
 			<picker class="picker" @change="bindPickerChange" :value="index" :range="array">
-				<view class="text">{{ array[index] }}</view>
+				<view class="text">{{ array[index]||'请选择退款原因' }}</view>
 			</picker>
 			<image class="icon" src="../../static/train/train-icon-05.png" mode="aspectFit"></image>
 		</view>
@@ -39,92 +39,26 @@
 </template>
 
 <script>
-import { getOrderDetail, getRefundReason, refundOrder, deleteImg } from '@/api/order.js';
-export default {
-	data() {
-		return {
-			orderDetail: null,
-			array: [],
-			index: 0,
-			refund_reason_wap_explain: '',
-			picList: []
-		};
-	},
-	onLoad(option) {
-		getOrderDetail(option.id).then(
-			result => {
-				this.orderDetail = result;
-			},
-			err => {
-				uni.showToast({
-					icon: 'none',
-					title: err.text
-				});
-			}
-		);
-		getRefundReason().then(
-			result => {
-				this.array = result;
-			},
-			err => {
-				uni.showToast({
-					icon: 'none',
-					title: err.text
-				});
-			}
-		);
-	},
-	methods: {
-		bindPickerChange: function(e) {
-			this.index = e.target.value;
+	import {
+		getOrderDetail,
+		getRefundReason,
+		refundOrder,
+		deleteImg
+	} from '@/api/order.js';
+	export default {
+		data() {
+			return {
+				orderDetail: null,
+				array: [],
+				index: -1,
+				refund_reason_wap_explain: '',
+				picList: []
+			};
 		},
-		upload() {
-			const this_ = this;
-			uni.chooseImage({
-				count: 3,
-				success: function(res) {
-					// this_.picList = this_.picList.concat(res.tempFilePaths);
-					// console.log(JSON.stringify(res.tempFilePaths));
-					const token = uni.getStorageSync('token');
-					for (let i = 0; i < res.tempFilePaths.length; i++) {
-						uni.uploadFile({
-							url: 'https://shops.yulongtianzi.com/api/upload/image',
-							filePath: res.tempFilePaths[i],
-							name: 'pics',
-							formData: {
-								filename: 'pics'
-							},
-							header: {
-								'Content-Type': 'multipart/form-data',
-								'Authori-zation': 'Bearer ' + token
-							},
-							success: function(res) {
-								const data = JSON.parse(res.data);
-								if (data.status == 200) {
-									this_.picList.push(data.data.url);
-								}
-							}
-						});
-					}
-				}
-			});
-		},
-		deleteImg(item) {
-			let index = this.picList.indexOf(item);
-			this.picList.splice(index, 1);
-		},
-		handleClick() {
-			refundOrder({
-				text: this.array[this.index],
-				uni: this.orderDetail.order_id,
-				refund_reason_wap_img: this.picList.join(','),
-				refund_reason_wap_explain: this.refund_reason_wap_explain,
-				order_status: '-1'
-			}).then(
+		onLoad(option) {
+			getOrderDetail(option.id).then(
 				result => {
-					uni.navigateBack({
-						delta: 1
-					});
+					this.orderDetail = result;
 				},
 				err => {
 					uni.showToast({
@@ -133,106 +67,325 @@ export default {
 					});
 				}
 			);
+			getRefundReason().then(
+				result => {
+					this.array = result;
+				},
+				err => {
+					uni.showToast({
+						icon: 'none',
+						title: err.text
+					});
+				}
+			);
+		},
+		methods: {
+			bindPickerChange: function(e) {
+				this.index = e.target.value;
+			},
+			upload() {
+				const this_ = this;
+				uni.chooseImage({
+					count: 3,
+					success: function(res) {
+						// this_.picList = this_.picList.concat(res.tempFilePaths);
+						// console.log(JSON.stringify(res.tempFilePaths));
+						const token = uni.getStorageSync('token');
+						for (let i = 0; i < res.tempFilePaths.length; i++) {
+							uni.uploadFile({
+								url: 'https://shops.yulongtianzi.com/api/upload/image',
+								filePath: res.tempFilePaths[i],
+								name: 'pics',
+								formData: {
+									filename: 'pics'
+								},
+								header: {
+									'Content-Type': 'multipart/form-data',
+									'Authori-zation': 'Bearer ' + token
+								},
+								success: function(res) {
+									const data = JSON.parse(res.data);
+									if (data.status == 200) {
+										this_.picList.push(data.data.url);
+									}
+								}
+							});
+						}
+					}
+				});
+			},
+			deleteImg(item) {
+				let index = this.picList.indexOf(item);
+				this.picList.splice(index, 1);
+			},
+			handleClick() {
+				if (!this.array[this.index]) {
+					uni.showToast({
+						icon: 'none',
+						title: '请选择退款原因'
+					});
+					return false;
+				}
+				refundOrder({
+					text: this.array[this.index],
+					uni: this.orderDetail.order_id,
+					refund_reason_wap_img: this.picList.join(','),
+					refund_reason_wap_explain: this.refund_reason_wap_explain,
+					order_status: '-1'
+				}).then(
+					result => {
+						uni.navigateBack({
+							delta: 1
+						});
+					},
+					err => {
+						uni.showToast({
+							icon: 'none',
+							title: err.text
+						});
+					}
+				);
+			}
 		}
-	}
-};
+	};
 </script>
 
 <style lang="less">
-.page-refund {
-	padding-top: 30rpx;
-	background-color: #f7f6f9;
+	.page-refund {
+		padding-top: 30rpx;
+		background-color: #f7f6f9;
 
-	.product-content {
-		position: relative;
-		display: flex;
-		align-items: center;
-		padding: 32rpx;
-		margin-bottom: 30rpx;
-		background-color: #fff;
-
-		.pic {
-			width: 160rpx;
-			height: 160rpx;
-			margin-right: 30rpx;
-			border-radius: 16rpx;
-			flex-shrink: 0;
-		}
-
-		.detail {
+		.product-content {
 			position: relative;
 			display: flex;
-			flex-direction: column;
-			justify-content: space-between;
-			flex: 1;
-			height: 160rpx;
+			align-items: center;
+			padding: 32rpx;
+			margin-bottom: 30rpx;
+			background-color: #fff;
 
-			.name {
-				width: 80%;
-				font-size: 28rpx;
-				font-family: PingFangSC-Regular, PingFang SC;
-				font-weight: 400;
-				color: #333;
-				line-height: 40rpx;
-				word-break: break-all;
-				text-overflow: ellipsis;
-				display: -webkit-box;
-				-webkit-box-orient: vertical;
-				-webkit-line-clamp: 2;
-				overflow: hidden;
+			.pic {
+				width: 160rpx;
+				height: 160rpx;
+				margin-right: 30rpx;
+				border-radius: 16rpx;
+				flex-shrink: 0;
 			}
 
-			.specs {
-				font-size: 24rpx;
-				font-family: PingFangSC-Regular, PingFang SC;
-				font-weight: 400;
-				color: #999;
-				line-height: 34rpx;
-			}
+			.detail {
+				position: relative;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-between;
+				flex: 1;
+				height: 160rpx;
 
-			.count {
-				position: absolute;
-				bottom: 0;
-				right: 0;
-				font-size: 24rpx;
-				font-family: PingFangSC-Regular, PingFang SC;
-				font-weight: 400;
-				color: #999;
-				line-height: 34rpx;
-			}
+				.name {
+					width: 80%;
+					font-size: 28rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #333;
+					line-height: 40rpx;
+					word-break: break-all;
+					text-overflow: ellipsis;
+					display: -webkit-box;
+					-webkit-box-orient: vertical;
+					-webkit-line-clamp: 2;
+					overflow: hidden;
+				}
 
-			.step {
-				position: absolute;
-				top: 0;
-				right: 0;
-				font-size: 28rpx;
-				font-family: PingFangSC-Regular, PingFang SC;
-				font-weight: 400;
-				color: #999;
-				line-height: 40rpx;
-
-				&::after {
-					content: '步';
-					font-size: 18rpx;
+				.specs {
+					font-size: 24rpx;
 					font-family: PingFangSC-Regular, PingFang SC;
 					font-weight: 400;
 					color: #999;
-					line-height: 26rpx;
+					line-height: 34rpx;
+				}
+
+				.count {
+					position: absolute;
+					bottom: 0;
+					right: 0;
+					font-size: 24rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #999;
+					line-height: 34rpx;
+				}
+
+				.step {
+					position: absolute;
+					top: 0;
+					right: 0;
+					font-size: 28rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #999;
+					line-height: 40rpx;
+
+					&::after {
+						content: '步';
+						font-size: 18rpx;
+						font-family: PingFangSC-Regular, PingFang SC;
+						font-weight: 400;
+						color: #999;
+						line-height: 26rpx;
+					}
+				}
+
+				.price {
+					position: absolute;
+					top: 0;
+					right: 0;
+					font-size: 28rpx;
+					font-family: PingFangSC-Regular, PingFang SC;
+					font-weight: 400;
+					color: #999;
+					line-height: 40rpx;
+
+					&::before {
+						content: '￥';
+						font-size: 24rpx;
+						font-family: PingFangSC-Regular, PingFang SC;
+						font-weight: 400;
+						color: #999;
+						line-height: 34rpx;
+					}
 				}
 			}
+		}
 
-			.price {
-				position: absolute;
-				top: 0;
-				right: 0;
+		.reason-content {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 0 32rpx 0 60rpx;
+			margin: 30rpx 0;
+			background-color: #ffffff;
+
+			.label {
+				width: 112rpx;
 				font-size: 28rpx;
 				font-family: PingFangSC-Regular, PingFang SC;
 				font-weight: 400;
-				color: #999;
+				color: #333333;
 				line-height: 40rpx;
+			}
 
-				&::before {
-					content: '￥';
+			.picker {
+				flex: 1;
+				height: 96rpx;
+				line-height: 96rpx;
+			}
+
+			.text {
+				height: 96rpx;
+				line-height: 96rpx;
+				font-size: 28rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #999999;
+				text-align: right;
+			}
+
+			.icon {
+				width: 19rpx;
+				height: 30rpx;
+				margin-left: 26rpx;
+			}
+		}
+
+		.desc-content {
+			position: relative;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 0 32rpx 0 60rpx;
+			background-color: #fff;
+
+			.label {
+				white-space: nowrap;
+				font-size: 28rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #333333;
+				line-height: 40rpx;
+			}
+
+			.text {
+				height: 96rpx;
+				line-height: 96rpx;
+				font-size: 28rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #999999;
+				text-align: right;
+			}
+		}
+
+		.pic-content {
+			padding: 30rpx 32rpx 30rpx 60rpx;
+			margin: 30rpx 0;
+			background-color: #ffffff;
+
+			.label {
+				margin-bottom: 20rpx;
+				white-space: nowrap;
+				font-size: 28rpx;
+				font-family: PingFangSC-Regular, PingFang SC;
+				font-weight: 400;
+				color: #333333;
+				line-height: 40rpx;
+			}
+
+			.pic-list {
+				display: flex;
+				align-items: center;
+				flex-wrap: wrap;
+			}
+
+			.pic-item {
+				position: relative;
+				width: 156rpx;
+				height: 156rpx;
+				margin-right: 14rpx;
+				margin-bottom: 14rpx;
+				border: 2rpx solid #ddd;
+				box-sizing: border-box;
+
+				.pic {
+					width: 156rpx;
+					height: 156rpx;
+					border-radius: 20rpx;
+				}
+			}
+
+			.btn-delete {
+				position: absolute;
+				top: -20rpx;
+				right: -20rpx;
+				width: 40rpx;
+				height: 40rpx;
+				background: url(../../static/order/order-icon-13.png) no-repeat center;
+				background-size: 40rpx 40rpx;
+			}
+
+			.btn {
+				width: 156rpx;
+				height: 156rpx;
+				padding: 30rpx 0;
+				margin-right: 14rpx;
+				margin-bottom: 14rpx;
+				border: 2rpx solid #ddd;
+				box-sizing: border-box;
+				text-align: center;
+
+				.icon {
+					width: 50rpx;
+					height: 50rpx;
+				}
+
+				.text {
 					font-size: 24rpx;
 					font-family: PingFangSC-Regular, PingFang SC;
 					font-weight: 400;
@@ -241,164 +394,23 @@ export default {
 				}
 			}
 		}
-	}
 
-	.reason-content {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0 32rpx 0 60rpx;
-		margin: 30rpx 0;
-		background-color: #ffffff;
-
-		.label {
-			width: 112rpx;
-			font-size: 28rpx;
-			font-family: PingFangSC-Regular, PingFang SC;
-			font-weight: 400;
-			color: #333333;
-			line-height: 40rpx;
-		}
-
-		.picker {
-			flex: 1;
-			height: 96rpx;
-			line-height: 96rpx;
-		}
-
-		.text {
-			height: 96rpx;
-			line-height: 96rpx;
-			font-size: 28rpx;
-			font-family: PingFangSC-Regular, PingFang SC;
-			font-weight: 400;
-			color: #999999;
-			text-align: right;
-		}
-
-		.icon {
-			width: 19rpx;
-			height: 30rpx;
-			margin-left: 26rpx;
-		}
-	}
-
-	.desc-content {
-		position: relative;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0 32rpx 0 60rpx;
-		background-color: #fff;
-
-		.label {
-			white-space: nowrap;
-			font-size: 28rpx;
-			font-family: PingFangSC-Regular, PingFang SC;
-			font-weight: 400;
-			color: #333333;
-			line-height: 40rpx;
-		}
-
-		.text {
-			height: 96rpx;
-			line-height: 96rpx;
-			font-size: 28rpx;
-			font-family: PingFangSC-Regular, PingFang SC;
-			font-weight: 400;
-			color: #999999;
-			text-align: right;
-		}
-	}
-
-	.pic-content {
-		padding: 30rpx 32rpx 30rpx 60rpx;
-		margin: 30rpx 0;
-		background-color: #ffffff;
-
-		.label {
-			margin-bottom: 20rpx;
-			white-space: nowrap;
-			font-size: 28rpx;
-			font-family: PingFangSC-Regular, PingFang SC;
-			font-weight: 400;
-			color: #333333;
-			line-height: 40rpx;
-		}
-
-		.pic-list {
-			display: flex;
-			align-items: center;
-			flex-wrap: wrap;
-		}
-
-		.pic-item {
-			position: relative;
-			width: 156rpx;
-			height: 156rpx;
-			margin-right: 14rpx;
-			margin-bottom: 14rpx;
-			border: 2rpx solid #ddd;
-			box-sizing: border-box;
-
-			.pic {
-				width: 156rpx;
-				height: 156rpx;
-				border-radius: 20rpx;
-			}
-		}
-
-		.btn-delete {
+		&>.btn {
 			position: absolute;
-			top: -20rpx;
-			right: -20rpx;
-			width: 40rpx;
-			height: 40rpx;
-			background: url(../../static/order/order-icon-13.png) no-repeat center;
-			background-size: 40rpx 40rpx;
-		}
-
-		.btn {
-			width: 156rpx;
-			height: 156rpx;
-			padding: 30rpx 0;
-			margin-right: 14rpx;
-			margin-bottom: 14rpx;
-			border: 2rpx solid #ddd;
-			box-sizing: border-box;
+			left: 0;
+			right: 0;
+			bottom: 102rpx;
+			width: 226rpx;
+			height: 72rpx;
+			margin: 0 auto;
+			background: #2975ff;
+			border-radius: 4rpx;
+			font-size: 28rpx;
+			font-family: PingFangSC-Semibold, PingFang SC;
+			font-weight: 600;
+			color: #ffffff;
+			line-height: 72rpx;
 			text-align: center;
-
-			.icon {
-				width: 50rpx;
-				height: 50rpx;
-			}
-
-			.text {
-				font-size: 24rpx;
-				font-family: PingFangSC-Regular, PingFang SC;
-				font-weight: 400;
-				color: #999;
-				line-height: 34rpx;
-			}
 		}
 	}
-
-	& > .btn {
-		position: absolute;
-		left: 0;
-		right: 0;
-		bottom: 102rpx;
-		width: 226rpx;
-		height: 72rpx;
-		margin: 0 auto;
-		background: #2975ff;
-		border-radius: 4rpx;
-		font-size: 28rpx;
-		font-family: PingFangSC-Semibold, PingFang SC;
-		font-weight: 600;
-		color: #ffffff;
-		line-height: 72rpx;
-		text-align: center;
-	}
-}
 </style>
